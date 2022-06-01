9bb1b2ceb83cadd5e52b99ecb2f3e4d25d68db64
Abhinav <abhinav.sattiraju@gmail.com>
2022-05-31 23:57:11 -0400

    refactored code according to UML

---
+++ CarFactory.py
@@ -0,0 +1,33 @@
+from CapuletEngine import CapuletEngine
+from NubbinBattery import NubbinBattery
+from SpindlerBattery import SpindlerBattery
+from SternmanEngine import SternmanEngine
+from WilloughbyEngine import WilloughbyEngine
+from car import Car
+
+
+class CarFactory:
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    def create_palindrome(current_date, last_service_date, warning_light_on):
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
+
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        return Car(engine, battery)
